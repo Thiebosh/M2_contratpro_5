ff8503921e59e9f706611c77ed9bc237780d4949
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2021-10-21 18:45:00 +0200

    better implementation

--- brique2/flex.cpp
+++ brique2/flex.cpp
@@ -1,11 +1,18 @@
 %option noyywrap
 %{
     #include "compiled.bison.cpp"
-    #include <string>
+
+    char* unquote(char* input, int length);
 %}
 
-TEXT    [0-9A-Za-z_ ]*
+QUOTE    "\""
 HEXA    [0-9a-fA-F]
+
+KEY_    {QUOTE}
+_KEY    {QUOTE}:
+
+TEXT    {QUOTE}[0-9A-Za-z_ ]*{QUOTE}
+COLOR   {QUOTE}#({HEXA}{6}|{HEXA}{3}){QUOTE}
 EOL     \r\n|\r|\n
 
 %%
@@ -16,24 +23,29 @@ EOL     \r\n|\r|\n
 "]" { cout << " ]"; return CLOSE_ARRAY; }
 ,   { cout << " ,"; return NEXT; }
 
-"\"root\":"         { cout << " root"; return ROOT; }
-"\"screen\":"       { cout << " screen"; return SCREEN; }
-"\"name\":"         { cout << " name"; return NAME; }
-"\"style\":"        { cout << " style"; return STYLE; }
-"\"align\":"        { cout << " align"; return ALIGN; }
-"\"color\":"        { cout << " color"; return COLOR; }
-"\"decoration\":"   { cout << " decoration"; return DECO; }
-"\"content\":"      { cout << " content"; return CONTENT; }
-"\"block\":"        { cout << " block"; return BLOCK; }
-"\"text\":"         { cout << " text"; return TEXT; }
-"\"value\":"        { cout << " value"; return VALUE; }
+{KEY_}root{_KEY}         { cout << " root"; return ROOT; }
+{KEY_}screen{_KEY}       { cout << " screen"; return SCREEN; }
+{KEY_}name{_KEY}         { cout << " name"; return NAME; }
+{KEY_}style{_KEY}        { cout << " style"; return STYLE; }
+{KEY_}align{_KEY}        { cout << " align"; return ALIGN; }
+{KEY_}color{_KEY}        { cout << " color"; return COLOR; }
+{KEY_}decoration{_KEY}   { cout << " decoration"; return DECO; }
+{KEY_}content{_KEY}      { cout << " content"; return CONTENT; }
+{KEY_}block{_KEY}        { cout << " block"; return BLOCK; }
+{KEY_}text{_KEY}         { cout << " text"; return TEXT; }
+{KEY_}value{_KEY}        { cout << " value"; return VALUE; }
 
-["]{TEXT}["]                     { yylval.string = strdup(yytext); yylval.string++[yyleng-1] = '\0'; cout << " str : " << yylval.string; return STR_VALUE; }
-["]#({HEXA}{6}|{HEXA}{3})["]  { yylval.string = strdup(yytext); yylval.string++[yyleng-1] = '\0'; cout << " str : " << yylval.string; return COLOR_VALUE; }
+{TEXT}  { yylval.string = unquote(yytext,yyleng); cout << " str : " << yylval.string; return STR_VALUE; }
+{COLOR} { yylval.string = unquote(yytext,yyleng); cout << " str : " << yylval.string; return COLOR_VALUE; }
 
-[ ]     { cout << " "; }
-\t      { cout << " tab"; }
+[ \t]   { cout << " "; }
 {EOL}   { cout << endl; }
-.       { cout << " raw text : " << yytext[0]; return yytext[0]; }
+.       { cout << endl << "Error - raw text : '" << yytext[0] << "'" << endl; exit(1); }
 
 %%
+
+char* unquote(char* input, int length) {
+    char* tmp = strdup(input);
+    tmp++[length-1] = '\0';
+    return tmp;
+}
