d398fa082aeca9848fc53f755651195ff937ef80
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2022-01-16 12:59:57 +0100

    project full tests

--- http_server/test/blueprints/test_project.py
+++ http_server/test/blueprints/test_project.py
@@ -1,40 +1,73 @@
 import pytest
-from quart import Quart, Response
+from quart import Quart, Response, json
 from flask_api import status
-from ..mocks.mongo_partner import DOC_LIST
+from ..mocks.mongo_partner import MongoPartnerMock, DOC_LIST
 
 ID = "61e131ce9c11b699edc38a1e"
+ID_LIST = [ID, ID]
 
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("app")
 async def test_project_create_ok(app: Quart) -> None:
-    client = app.test_client()
+    class db_mock(MongoPartnerMock):
+        async def find_one(self, *_):
+            return False
 
-    # mocker ici l'objet db...
+    app.config["partners"]["db"] = db_mock()
 
     form = {
         "name": "some",
-        "users_id": [ID, ID]
+        "users_id": json.dumps(ID_LIST)
     }
-    response:Response = await client.post("/project/create", form=form)
+    response:Response = await app.test_client().post("/project/create", form=form)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.mimetype == "application/json"
+    assert (await response.get_json())["success"] is True
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("app")
+async def test_project_create_not_ok(app: Quart) -> None:
+    class db_mock(MongoPartnerMock):
+        async def find_one(self, *_):
+            return False
+
+        async def insert_one(self, *_):
+            return False
+
+    app.config["partners"]["db"] = db_mock()
+
+    form = {
+        "name": "some",
+        "users_id": json.dumps(ID_LIST)
+    }
+    response:Response = await app.test_client().post("/project/create", form=form)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.mimetype == "application/json"
+    assert (await response.get_json())["success"] is False
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("app")
+async def test_project_create_exist(app: Quart) -> None:
+    form = {
+        "name": "some",
+        "users_id": json.dumps(ID_LIST)
+    }
+    response:Response = await app.test_client().post("/project/create", form=form)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.mimetype == "application/json"
     assert (await response.get_json())["success"] == "already exist"
-    # assert (await response.get_json())["success"] is True
 
 
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("app")
 async def test_project_search_by_user_ok(app: Quart) -> None:
-    client = app.test_client()
-
-    # mocker ici l'objet db...
-
     form = {
         "id": ID
     }
-    response:Response = await client.post("/project/search_by_user", form=form)
+    response:Response = await app.test_client().post("/project/search_by_user", form=form)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.mimetype == "application/json"
@@ -44,33 +77,44 @@ async def test_project_search_by_user_ok(app: Quart) -> None:
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("app")
 async def test_project_add_user_ok(app: Quart) -> None:
-    client = app.test_client()
+    form = {
+        "id": ID,
+        "user_id": ID
+    }
+    response:Response = await app.test_client().post("/project/add_user", form=form)
 
-    # mocker ici l'objet db...
+    assert response.status_code == status.HTTP_200_OK
+    assert response.mimetype == "application/json"
+    assert (await response.get_json())["success"] is True
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("app")
+async def test_project_add_user_not_ok(app: Quart) -> None:
+    class db_mock(MongoPartnerMock):
+        async def update_one(self, *_):
+            return False
+
+    app.config["partners"]["db"] = db_mock()
 
     form = {
         "id": ID,
         "user_id": ID
     }
-    response:Response = await client.post("/project/add_user", form=form)
+    response:Response = await app.test_client().post("/project/add_user", form=form)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.mimetype == "application/json"
-    assert (await response.get_json())["success"] is True
+    assert (await response.get_json())["success"] is False
 
 
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("app")
 async def test_project_remove_user_ok(app: Quart) -> None:
-    client = app.test_client()
-
-    # mocker ici l'objet db...
-
     form = {
         "id": ID,
         "user_id": ID
     }
-    response:Response = await client.post("/project/remove_user", form=form)
+    response:Response = await app.test_client().post("/project/remove_user", form=form)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.mimetype == "application/json"
@@ -79,16 +123,50 @@ async def test_project_remove_user_ok(app: Quart) -> None:
 
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("app")
+async def test_project_remove_user_not_ok(app: Quart) -> None:
+    class db_mock(MongoPartnerMock):
+        async def update_one(self, *_):
+            return False
+
+    app.config["partners"]["db"] = db_mock()
+
+    form = {
+        "id": ID,
+        "user_id": ID
+    }
+    response:Response = await app.test_client().post("/project/remove_user", form=form)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.mimetype == "application/json"
+    assert (await response.get_json())["success"] is False
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("app")
 async def test_project_delete_ok(app: Quart) -> None:
-    client = app.test_client()
-
-    # mocker ici l'objet db...
-
     form = {
         "id": ID
     }
-    response:Response = await client.post("/project/delete", form=form)
+    response:Response = await app.test_client().post("/project/delete", form=form)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.mimetype == "application/json"
     assert (await response.get_json())["success"] is True
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("app")
+async def test_project_delete_not_ok(app: Quart) -> None:
+    class db_mock(MongoPartnerMock):
+        async def delete_one(self, *_):
+            return False
+
+    app.config["partners"]["db"] = db_mock()
+
+    form = {
+        "id": ID
+    }
+    response:Response = await app.test_client().post("/project/delete", form=form)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.mimetype == "application/json"
+    assert (await response.get_json())["success"] is False
