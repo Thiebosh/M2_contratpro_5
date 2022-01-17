32530c3f0c095b56b16179d003e70bda2d6ecdd7
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2022-01-17 23:08:30 +0100

    dockerise websocket server

--- websocket_server/_run.sh
+++ websocket_server/_run.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+# verify that vsc end line sequence is LF
 sudo docker build . --tag="ws_serv"
-sudo docker run -dp 8010:5001 --env-file=.env --env-file=../.env ws_serv:latest
+# sudo docker run -dp 8010:5001 --env-file=.env --env-file=../.env ws_serv:latest
+
+# tmp : lie a la console et lance le serveur
+sudo docker run -p 8001:5001 --env-file=.env --env-file=../.env -ti ws_serv:latest /bin/sh
+# python .
