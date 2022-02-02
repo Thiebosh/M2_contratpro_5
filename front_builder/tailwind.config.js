7c592af2c0ff0a7c199f3e6ed62ad8023a0f80cf
Etienne S <43611141+ETN-Tech@users.noreply.github.com>
2022-02-02 17:07:00 +0100

    Add tailwindui package

--- front_builder/tailwind.config.js
+++ front_builder/tailwind.config.js
@@ -1,10 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./node_modules/@themesberg/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
     require('@themesberg/flowbite/plugin')
