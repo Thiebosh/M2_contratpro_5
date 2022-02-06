f3929237fb504ee5f23fc6e0aacf2ce4809771eb
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2022-02-06 17:29:13 +0100

    delete drive folder with project

--- http_server/test/conftest.py
+++ http_server/test/conftest.py
@@ -1,6 +1,7 @@
 from typing import AsyncGenerator
 
 from test.mocks.mongo_partner import MongoPartnerMock
+from test.mocks.drive_partner import DrivePartnerMock
 
 import pytest
 from quart import Quart
@@ -11,6 +12,6 @@ from src.app import create_app
 
 @pytest.fixture(scope="function")
 async def app() -> AsyncGenerator[Quart, None]:
-    _app:Quart = create_app(config=DevelopmentConfig, db=MongoPartnerMock())
+    _app:Quart = create_app(config=DevelopmentConfig, db=MongoPartnerMock(), nas=DrivePartnerMock())
     async with _app.test_app():
         yield _app
---
+++ http_server/test/mocks/drive_partner.py
@@ -0,0 +1,7 @@
+
+class DrivePartnerMock:
+    def __init__(self, *_):
+        pass
+
+    def remove_folder(self, *_):
+        return True
