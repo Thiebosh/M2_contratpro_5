474e2f152ceed345003d241f2e4637d716abbb18
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2022-02-09 10:55:02 +0100

    mock process output

--- websocket_server/src/partners/cpp_partner.py
+++ websocket_server/src/partners/cpp_partner.py
@@ -3,7 +3,7 @@ from subprocess import Popen, PIPE
 
 class CppPartner():
     def __init__(self) -> None:
-        self.exe_path = "./exe_path"
+        self.exe_path = "./cpp/prototypeur.exe"
         self.freq = 0.4
 
     def copy_partner(self):
@@ -19,13 +19,37 @@ class CppPartner():
                 break
             await asyncio.sleep(self.freq)
 
-        lines = process.communicate()[0].split("\n")
+        lines = process.communicate()[0].split("\n\n")
+        lines = [ # example
+            'ecran1.html \
+            <section style="background-color: #6897bb; margin: auto; "> \
+                <div style="margin: auto; "> \
+                    <p style="color: red; text-decoration: underline;"> \
+                        hello world \
+                    </p> \
+                    <div style="background-color: blue;"> \
+                        <p> \
+                            some nested text \
+                        </p> \
+                    </div> \
+                    <p style="color: red;"> \
+                        other text \
+                    </p> \
+                </div> \
+                <div style="margin: auto; "> \
+                    <p style="color: green; text-decoration: italic;"> \
+                        hello world1 \
+                    </p> \
+                    <p style="color: lightblue;"> \
+                        hello world2 \
+                    </p> \
+                </div> \
+            </section>'
+        ]
 
-        if lines[2] == "error":  # retcode and retcode != 0:  # execution error
-            print(f"cpp executable return error '{retcode}'")
-            print(lines)
-            return False
+        # if lines[0] == "error":  # retcode and retcode != 0:  # execution error
+        #     print(f"cpp executable return error '{retcode}'")
+        #     print(lines)
+        #     return False
 
-        # parse lines...
-
-        return [{"name": "exemple.html", "content": "hello world"}] # exemple
+        return [{"name": line.split("\n")[0], "content": line[line.find("\n")+1:]} for line in lines]
