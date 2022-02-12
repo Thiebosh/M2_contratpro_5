7747032424055e10b5bcc0897201df4a17a48f14
Delano Nyotue <veirel_delano.koyouo_nyotue@isen.yncrea.fr>
2022-02-12 18:39:06 +0100

    Creation des dossiers et fichiers sur notre serveurs

--- render_server/src/controllers/create_folder.php
+++ render_server/src/controllers/create_folder.php
@@ -1,2 +1,32 @@
 <?php
-//fonction / objet de création de folder OU fait l'action
+$emplacement = "root";
+if (isset($_GET['action']) && isset($_GET['object'])) {
+
+    switch (strtolower($_GET['action'])) {
+
+        case "creer":
+
+            switch (strtolower($_GET['object'])) {
+
+                case "dossier" :
+                    if (!mkdir(strtolower($_GET['emplacement']), 0777, true)) {
+                        die('Échec lors de la création des dossiers...');
+                    }
+                    echo "creation effectuer";
+                    break;
+
+                case "fichier" :
+                    if (!fopen(strtolower($emplacement) . 'index' . '.php', 'w+')) {
+                        die('Échec lors de la création du fichier...');
+                    }
+
+                    echo "creation effectuer";
+                    break;
+
+            }
+            break;
+
+    }
+} else {
+    echo "votre url est non valable Certains paramètres y sont absents. ";
+}
