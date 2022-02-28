71466eb2c3807454fa61f4089c847723f06ced01
delano99 <veirel_delano.koyouo_nyotue@student.junia.com>
2022-02-28 06:08:14 -0800

    create the constructeur directoryManager

--- render_server/src/index.php
+++ render_server/src/index.php
@@ -27,6 +27,8 @@ function generate() {
     return $SUCCESS;
 }
 
+$directoryManager = new DirectoryManager(__DIR__."/projects");
+
 if (isset($_GET['action'])) {
     switch ($action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING)) {
         case 'generate':
@@ -34,19 +36,19 @@ if (isset($_GET['action'])) {
             exit();
 
         case 'create_folder': //localhost:80/index.php?action=create_folder&project_name=test
-            http_response_code((new DirectoryManager)->create_folder());
+            http_response_code(($directoryManager)->create_folder());
             exit();
 
         case 'create_file': //localhost:80/index.php?action=create_file&project_name=test&file_name=exemple.html&file_content="un peu de texte"
-            http_response_code((new DirectoryManager)->create_folder());
+            http_response_code($directoryManager)->create_folder());
             exit();
 
         case 'remove_files': //localhost:80/index.php?action=remove_files&project_name=test
-            http_response_code((new DirectoryManager)->remove_files());
+            http_response_code($directoryManager)->remove_files());
             exit();
 
         case 'remove_folder': //localhost:80/index.php?action=remove_folder&project_name=test
-            http_response_code((new DirectoryManager)->remove_folder());
+            http_response_code($directoryManager)->remove_folder());
             exit();
 
         case "probe":
