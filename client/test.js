250f5d3df6cce0cfe69d23811b31f23a12f2ca53
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2022-03-01 14:47:28 +0100

    fix

--- client/test.js
+++ client/test.js
@@ -1,15 +1,13 @@
 $(function () {
     $.ajax({
-        url:"http//localhost:8001/account/connect",
+        url:"http://localhost:8001/account/connect",
         type:"POST",
         data:{
             name:"test",
             password:"test"
         },
-        //contentType : 'application/json; charset=utf-8',
-
         success: function(resp) {
-            console.log(resp);
+            console.log(resp.id);
         },
         error: function(){
             console.log("failure");
