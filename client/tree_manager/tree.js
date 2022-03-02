9cdad6bf5c1e864c1aa6de6ce7fdc04bebb079bf
Antoine <antoinea723@gmail.com>
2022-03-02 16:24:39 +0100

    WIP Init JSON tree

--- client/tree_manager/tree.js
+++ client/tree_manager/tree.js
@@ -1,8 +1,8 @@
 $(function () {
     // ************** Generate the tree diagram	 *****************
     margin = {top: 20, right: 120, bottom: 20, left: 120};
-    width = 960 - margin.right - margin.left;
-    height = 500 - margin.top - margin.bottom;
+    width = 1500;
+    height = 1000;
 
     i = 0;
     duration = 500;
@@ -43,50 +43,32 @@ $(function () {
 
     // update(root);
 
-    function nodeType(elem){
-        console.log(elem);
-    }
-
     function rec(syntax, node, parentNode){
         for (const key in node){
-            // console.log("KEY " + key)
-            // console.log(node)
-                if (Array.isArray(node[key])){ //On rencontre un JSON Array
-                    let arrayNode = parentNode.addArrayChild(key);
-                    // console.log(arrayNode)
-                    for (let i = 0; i < node[key].length; i++){
-                        // nodeType(node[key][i]);
-                        rec(syntax, node[key][i], arrayNode)
-                    }
-                } else if (typeof node[key] === "object"){ //On rencontre un JSON Object
-                    let objectNode = parentNode.addObjectChild(key);
-                    // nodeType(node[key])
-                    rec(syntax, node[key], objectNode);
-                } else{ //On rencontre une string (on arrive au bout de la branche de l'arbre)
-                    nodeType(node[key])
+            if (Array.isArray(node[key])){ //On rencontre un JSON Array
+                let arrayNode = parentNode.addArrayChild(key);
+                for (let i = 0; i < node[key].length; i++){
+                    let arrayChildNode = arrayNode.addObjectChild()
+                    rec(syntax, node[key][i], arrayChildNode)
+
                 }
+            } else if (typeof node[key] === "object"){ //On rencontre un JSON Object
+                let objectNode = parentNode.addObjectChild(key);
+                rec(syntax, node[key], objectNode);
+
+            } else{ //On rencontre une string (on arrive au bout de la branche de l'arbre)
+            }
         }
     }
 
     function createTreeFromJson(json){
     	$.getJSON("../syntax/syntax.json", function(syntax){
-    		// console.log(data)
             $.getJSON("json/example.json",function(json){
-                // console.log(syntax);
-                // console.log(json);
                 root = new MainNode();
-                // console.log(Object.keys(json["root"]))
                 rec(syntax, json["root"], root)
             });
     	});
-    	// $.getJSON("json/example.json",function(json){
-    	// 	root = new MainNode();
-
-    	// 	console.log(json[Object.keys(json)[0]]);
-    	// })
 
     }
     createTreeFromJson("");
-    // test = new MainNode();
-    // screens = test.addArrayChild("Screen");
 });
