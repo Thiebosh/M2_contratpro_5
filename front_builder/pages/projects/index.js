04b361280d8fe34bf2457c2d8491dbcab12949e5
Etienne S <43611141+ETN-Tech@users.noreply.github.com>
2022-03-02 18:52:45 +0100

    Add action modals

--- front_builder/pages/projects/index.js
+++ front_builder/pages/projects/index.js
@@ -5,8 +5,17 @@ import {
   Center,
   Container,
   Flex,
+  FormControl,
+  FormLabel,
   Heading,
   IconButton,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Select,
   Table,
   TableCaption,
@@ -26,11 +35,15 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import $ from "jquery";
 import requireAuth from "../../components/utils/requireAuth";
+import ProjectCreateModal from "../../components/modals/ProjectCreateModal";
+import ProjectRenameModal from "../../components/modals/ProjectRenameModal";
 
 const idUser = "61e131ce9c11b699edc38a1e";
 
-export default function Projects() {
+export default function Projects({ user }) {
   const [projects, setProjects] = useState([]);
+  const [openCreate, setOpenCreate] = useState(false);
+  const [renameProject, setRenameProject] = useState(0);
 
   useEffect(() => {
     const dataProjects = {
@@ -50,76 +63,7 @@ export default function Projects() {
     });
   }, []);
 
-  return (
-    <Container maxW={"container.xl"} py={8}>
-      <Flex m={5} justifyContent={"space-between"}>
-        <Wrap>
-          <WrapItem>
-            <Avatar size="xl" src="https://bit.ly/code-beast" />
-          </WrapItem>
-          <WrapItem>
-            <Center w="200px" h="100px">
-              <Heading>Projets</Heading>
-            </Center>
-          </WrapItem>
-        </Wrap>
-        <Box>
-          <Link href="/projects/create">
-            <Button leftIcon={<AddIcon />} colorScheme={"blue"}>
-              New project
-            </Button>
-          </Link>
-        </Box>
-      </Flex>
-
-      <Box className="container">
-        <Wrap direction="column">
-          <Table variant="simple">
-            <TableCaption>Liste des projets en cours</TableCaption>
-            <Thead>
-              <Tr>
-                <Th>Nom</Th>
-                <Th>Date de création</Th>
-                <Th>Dernière modification au</Th>
-                <Th>Actions</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {projects.map((project) => (
-                <Tr key={project._id}>
-                  <Td>{project.name}</Td>
-                  <Td>{dayjs(project.creation).format("D MMMM YYYY")}</Td>
-                  <Td>{dayjs(project.last_specs).format("D MMMM YYYY")}</Td>
-                  <Td>
-                    <Link href="/dashboard">
-                      <IconButton
-                        aria-label="modifier"
-                        mx={1}
-                        icon={<EditIcon />}
-                      />
-                    </Link>
-                    <Link href="/projects/settings">
-                      <IconButton
-                        aria-label="settings"
-                        mx={1}
-                        icon={<SettingsIcon />}
-                      />
-                    </Link>
-                    <IconButton
-                      aria-label="delete"
-                      mx={1}
-                      icon={<DeleteIcon />}
-                    />
-                  </Td>
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
-        </Wrap>
-      </Box>
-    </Container>
-  );
-
+  /* HANDLE FUNCTIONS */
   function deleteProject(projectID) {
     $.ajax({
       url: "http://localhost:8001/project/delete",
@@ -135,6 +79,87 @@ export default function Projects() {
       },
     });
   }
+
+  return (
+    <>
+      <Container maxW={"container.xl"} py={8}>
+        <Flex m={5} justifyContent={"space-between"}>
+          <Wrap>
+            <WrapItem>
+              <Avatar size="xl" src="https://bit.ly/code-beast" />
+            </WrapItem>
+            <WrapItem>
+              <Center w="200px" h="100px">
+                <Heading>Projets</Heading>
+              </Center>
+            </WrapItem>
+          </Wrap>
+          <Box>
+            <Button
+              leftIcon={<AddIcon />}
+              colorScheme={"blue"}
+              onClick={() => setOpenCreate(true)}
+            >
+              New project
+            </Button>
+          </Box>
+        </Flex>
+
+        <Box className="container">
+          <Wrap direction="column">
+            <Table variant="simple">
+              <Thead>
+                <Tr>
+                  <Th>Name</Th>
+                  <Th>Creation date</Th>
+                  <Th>Last modified</Th>
+                  <Th>Actions</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {projects.map((project) => (
+                  <Tr key={project.id}>
+                    <Td>{project.name}</Td>
+                    <Td>{dayjs(project.creation).format("D MMMM YYYY")}</Td>
+                    <Td>{dayjs(project.last_specs).format("D MMMM YYYY")}</Td>
+                    <Td>
+                      <IconButton
+                        aria-label="modifier"
+                        mx={1}
+                        icon={<EditIcon />}
+                        onClick={() => setRenameProject(project.id)}
+                      />
+                      <Link href={`/projects/${project.id}/settings`}>
+                        <IconButton
+                          aria-label="settings"
+                          mx={1}
+                          icon={<SettingsIcon />}
+                        />
+                      </Link>
+                      <IconButton
+                        aria-label="delete"
+                        mx={1}
+                        icon={<DeleteIcon />}
+                      />
+                    </Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </Wrap>
+        </Box>
+      </Container>
+      <ProjectCreateModal
+        isOpen={openCreate}
+        setIsOpen={setOpenCreate}
+        user={user}
+      />
+      <ProjectRenameModal
+        projectId={renameProject}
+        setProjectId={setRenameProject}
+      />
+    </>
+  );
 }
 
 export const getServerSideProps = requireAuth;
