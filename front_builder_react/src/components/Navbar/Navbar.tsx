5422c0669fdcdc8fde9e9e39b36896eebce6213d
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2022-04-12 22:18:17 +0200

    rename file + user menu

--- front_builder_react/src/components/Navbar/Navbar.tsx
+++ front_builder_react/src/components/Navbar/Navbar.tsx
@@ -1,22 +1,44 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
-import { removeSessionUser } from '../../session/user';
+import { getSessionUser, removeSessionUser } from '../../session/user';
 
 import './NavBar.scss';
 
-function User() {
+function Card() {
     return (
-        <div>
-            <FontAwesomeIcon className="user" icon={faCircleUser}/>
-            <a href="/user/login"><button>connexion</button></a>
-            <a href="/"><button onClick={removeSessionUser}>déconnexion</button></a>
+        <div className='card'>
+            {
+                getSessionUser() ? 
+                    <>
+                        <a href='/user/profile'>Account</a>
+                        <a href='/user/settings'>Settings</a>
+                        <hr/>
+                        <a href='/' onClick={removeSessionUser}>Logout</a>
+                    </>
+                    :
+                    <>
+                        <a href='/user/login'>Log in</a>
+                        <a href='/user/create'>Sign in</a>
+                    </>
+            }
+        </div>
+    );
+}
+
+function User() {
+    const [displayMenu, setDisplayMenu] = useState<boolean>(false);
+    return (
+        <div className='user' onClick={() => setDisplayMenu(!displayMenu)}>
+            <FontAwesomeIcon icon={faCircleUser}/>
+            { displayMenu && <Card/> }
         </div>
     );
 }
 
 export default function NavBar() {
     return (
-    <nav id="navbar">
+    <nav id='navbar'>
         <div className='left-part'>
             <a href='/home' className="title">
                 <div>
