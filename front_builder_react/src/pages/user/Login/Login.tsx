95c4475f5947a4201d40f6529b91ca80a2bf08f7
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2022-04-13 00:31:36 +0200

    login page done

--- front_builder_react/src/pages/user/Login/Login.tsx
+++ front_builder_react/src/pages/user/Login/Login.tsx
@@ -1,12 +1,73 @@
-import {setSessionUser} from '../../../session/user';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { postAccountConnect } from '../../../partners/rest';
+import {setSessionUser, getSessionUser} from '../../../session/user';
+
 import './Login.scss';
+
+function Card() {
+    const navigate = useNavigate();
+    const [name, setName] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [warnMsg, setWarnMsg] = useState<string>("");
+    const [errorMsg, setErrorMsg] = useState<string>("");
+
+    useEffect(() => {
+        if (warnMsg) {
+            setTimeout(() => setWarnMsg(""), 4000);
+        }
+    }, [warnMsg]);
+    useEffect(() => {
+        if (errorMsg) {
+            setTimeout(() => setErrorMsg(""), 4000);
+        }
+    }, [errorMsg]);
+
+    function triggerLogin() {
+        if (!(name && password)) {
+            setWarnMsg("Empty field(s)");
+            return;
+        }
+
+        postAccountConnect(name, password)
+        .then((data) => {
+            if (typeof data.id == "boolean") {
+                setErrorMsg("Credentials invalid");
+                return;
+            }
+            setSessionUser(data.id);
+        })
+        .then(() => navigate('/user/profile'))
+        .catch(error => {
+            setErrorMsg("Internal error");
+            console.log("Error:", error);
+        });
+    }
+
+    return (
+        <div className='card'>
+            <div className='input_group'>
+                <label>Username</label>
+                <br/>
+                <input type='text' onChange={(event) => setName(event.target.value)}/>
+            </div>
+            <div className='input_group'>
+                <label>Password</label>
+                <br/>
+                <input type='password'  onChange={(event) => setPassword(event.target.value)}/>
+            </div>
+            <div className='button' onClick={triggerLogin}>Sign in</div>
+            { warnMsg && <div className='warning'>{warnMsg}</div> }
+            { errorMsg && <div className='error'>{errorMsg}</div> }
+        </div>
+    );
+}
 
 export default function Login() {
     return (
-        <div id="login">
-            Login page
-            <a href="/user/profile"><button onClick={() => setSessionUser("some")}>valider</button></a>
-            <a href="/user/create"><button>créer compte</button></a>
-        </div>
+        <section id="login">
+            <h1>Login</h1>
+            <Card/>
+        </section>
     );
 }
