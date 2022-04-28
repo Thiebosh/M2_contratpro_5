1557f9ba26e03b5c2984b7a2807b6435d728aa60
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2022-04-29 00:27:28 +0200

    websocket connexion to test

--- front_builder_react/src/pages/Project/Proto/Proto.tsx
+++ front_builder_react/src/pages/Project/Proto/Proto.tsx
@@ -14,6 +14,31 @@ export function Proto() {
     useEffect(() => {
         postProjectExistForUser(userContext.user, urlName || "")
         .then((data) => data.id || navigate('/projects'))
+        .then(() => {
+            const websocket = new WebSocket('ws://127.0.0.1:8002')
+            websocket.onopen = () => {
+                console.log('connected')
+                const msg = JSON.stringify({"action":"connectRoom", "roomName" : urlName, "author": userContext.user}) // à traduire en nom ou le stocker
+                websocket.send(msg);
+            }
+            websocket.onmessage = (event) => {
+                const data = JSON.parse(event.data)
+                console.log(data);
+            }
+            websocket.onclose = function(event) {
+                if (event.wasClean) {
+                    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+                } else {
+                    // e.g. server process killed or network down
+                    // event.code is usually 1006 in this case
+                    console.log('[close] Connection died');
+                }
+            };
+            websocket.onerror = function(error) {
+                websocket.close();
+                console.log(`[error] ${error}`);
+            };
+        })
         .catch(error => {
             // setErrorMsg("Internal error");
             console.log("Error:", error);
