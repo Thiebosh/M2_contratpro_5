e05fc3f69c6de680dd89aeb9fdc36ef20771ab0b
Antoine <antoinea723@gmail.com>
2022-05-16 21:31:15 +0200

    Remove unused function

--- front_builder_react/src/components/Modal/Modal.tsx
+++ front_builder_react/src/components/Modal/Modal.tsx
@@ -14,7 +14,7 @@ export function Modal(props:ModalProps){
         <div className="modal">
             <header className="modal-header">
                 <h2>Modal Title</h2>
-                <button onClick={() => closeModal(props.openClose)} className="close-button">&times;</button>
+                <button onClick={() => props.openClose(false)} className="close-button">&times;</button>
             </header>
             <main className="modal-main">
                 <div id="modal-content">
@@ -25,8 +25,4 @@ export function Modal(props:ModalProps){
         </div>
     </>
     )
-}
-
-function closeModal(openClose:Function){
-    openClose(false)
 }
