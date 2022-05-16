db2feb6261b3a87c2f61d92f30df4ac03bd2b125
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2022-05-17 00:04:52 +0200

    rest: half industrialized

---
+++ rest_server/src/defines.py
@@ -0,0 +1,52 @@
+import os
+
+# OS environment
+OS_HOST=os.environ.get("HOST", '0.0.0.0')
+OS_PORT=int(os.environ.get("PORT", default=5000))
+
+OS_MONGO_USERNAME=os.environ.get("MONGO_USERNAME")
+OS_MONGO_PASSWORD=os.environ.get("MONGO_PASSWORD")
+OS_MONGO_URL=os.environ.get("MONGO_URL")
+
+OS_IS_LOCAL=os.environ.get("IS_LOCAL", default="False")
+
+# DICT KEYS
+PARTNERS="partners"
+DB="db"
+NAS="nas"
+CRYPT="crypt"
+
+# LOGGIN MSGS
+INIT_PARTNERS="init: setup partners"
+INIT_ENDPOINTS="inti: load blueprints"
+INIT_DONE="init: done"
+
+ENDPOINT_CALL="call"
+
+ARGS_INVALID_NUMBER="args: invalid number"
+ARGS_INVALID_PARSE="args: invalid name or format"
+ARGS_INVALID_VALUE="args: invalid value"
+ARGS_OVERLOAD_VALUE="args: overload value"
+ARGS_INVALID_JSON="args: invalid json format"
+ARGS_INVALID_OBJECT_ID="args: invalid objectId"
+
+MONGO_PARTNER_EXCEPTION="mongo partner failure: %s"
+MONGO_PARTNER_TIMEOUT="mongo partner: timeout - check wifi"
+
+NO_SYNTAX="no syntax found in db"
+NO_ACCOUNT="no account found (%s)"
+
+# RETURN MSGS
+MSG_ARGS_INVALID_NUMBER="incorrect nb of params"
+MSG_ARGS_INVALID_PARSE="invalid params name or format"
+MSG_ARGS_INVALID_VALUE="invalid params value"
+MSG_ARGS_INVALID_JSON="invalid json params"
+MSG_ARGS_INVALID_OBJECT_ID="invalid objectId"
+
+MSG_MONGO_PARTNER_EXCEPTION="db request error"
+MSG_MONGO_PARTNER_TIMEOUT="operation timeout"
+
+MSG_NO_SYNTAX="no syntax found"
+MSG_NO_ACCOUNT="no account found"
+
+MSG_ALREADY_EXIST={"success": "already exist"}
