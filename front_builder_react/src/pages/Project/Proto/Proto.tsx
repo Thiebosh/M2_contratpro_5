f6fa8ce580365ebc1fb727cd9f766ec6c3823866
Grosbety <benjamin.grosbety@isen.yncrea.fr>
2022-05-19 11:40:01 +0200

    select page proto

--- front_builder_react/src/pages/Project/Proto/Proto.tsx
+++ front_builder_react/src/pages/Project/Proto/Proto.tsx
@@ -1,43 +1,43 @@
-import { useEffect, useState } from 'react';
-import { Fade } from 'react-awesome-reveal';
-import { useNavigate, useParams } from 'react-router-dom';
-import { init_websocket } from '../../..';
-import { Collabs } from '../../../components/Collabs';
+import {useEffect, useState} from 'react';
+import {Fade} from 'react-awesome-reveal';
+import {useNavigate, useParams} from 'react-router-dom';
+import {init_websocket} from '../../..';
+import {Collabs} from '../../../components/Collabs';
 
-import { postProjectExistForUser, postProjectGetProtoPages } from '../../../partners/rest';
-import { useUserContext } from '../../../session/user';
+import {postProjectExistForUser, postProjectGetProtoPages} from '../../../partners/rest';
+import {useUserContext} from '../../../session/user';
 
 import './Proto.scss';
 
 export function Proto() {
     const navigate = useNavigate();
-    const { urlName } = useParams();
+    const {urlName} = useParams();
     const userContext = useUserContext();
     const [projectId, setProjectId] = useState<string>();
 
     useEffect(() => {
         postProjectExistForUser(userContext.user.id, urlName || "")
-        .then((data) => {
-            setProjectId(data.project_id);
-        })
-        .catch(error => {
-            // setErrorMsg("Internal error");
-            console.log("Error:", error);
-            navigate('/projects');
-        });
+            .then((data) => {
+                setProjectId(data.project_id);
+            })
+            .catch(error => {
+                // setErrorMsg("Internal error");
+                console.log("Error:", error);
+                navigate('/projects');
+            });
     }, [userContext.user, urlName, navigate]);
 
-    const [pages, setPages] = useState<{link:string, name:string}[]>([{name:'Default', link:''}]);
+    const [pages, setPages] = useState<{ link: string, name: string }[]>([{name: 'Default', link: ''}]);
     const [currentPage, setCurrentPage] = useState<string>("");
 
     useEffect(() => {
         if (!projectId) return;
         postProjectGetProtoPages(projectId)
-        .then((data) => setPages([{name:'404', link:''}, ...data.pages]))
-        .catch(error => {
-            // setErrorMsg("Internal error");
-            console.log("Error:", error);
-        });
+            .then((data) => setPages([{name: '404', link: ''}, ...data.pages]))
+            .catch(error => {
+                // setErrorMsg("Internal error");
+                console.log("Error:", error);
+            });
     }, [projectId]);
 
     const [socket, setSocket] = useState<WebSocket>();
@@ -49,6 +49,7 @@ export function Proto() {
     const [initCollab, setInitCollab] = useState<string[]>([]);
     const [addCollab, setAddCollab] = useState<string>();
     const [removeCollab, setRemoveCollab] = useState<string>();
+
     useEffect(() => {
         if (initCollab.length) {
             setLoggedCollabs(initCollab);
@@ -80,7 +81,7 @@ export function Proto() {
         socket.onmessage = (event) => {
             const data = JSON.parse(event.data)
             console.log(data);
-            switch(Object.keys(data)[0]) {
+            switch (Object.keys(data)[0]) {
                 case 'init_collabs':
                     setInitCollab(data["init_collabs"] as string[]);
                     break;
@@ -102,27 +103,26 @@ export function Proto() {
                         setProtoUpdate(true);
                         setLoadingPage(true);
                         setInfoMsg("New prototype : reloading");
-                        setPages([{name:'404', link:''}]);
-                    }
-                    else {
+                        setPages([{name: '404', link: ''}]);
+                    } else {
                         postProjectGetProtoPages(projectId as string)
-                        .then((data) => {
-                            setPages([{name:'404', link:''}, ...data.pages]);
-                            setSuccessMsg("New prototype : ready to use");
-                            setLoadingPage(false);
-                            setProtoUpdate(false);
-                        })
-                        .catch(error => {
-                            // setErrorMsg("Internal error");
-                            console.log("Error:", error);
-                        });
+                            .then((data) => {
+                                setPages([{name: '404', link: ''}, ...data.pages]);
+                                setSuccessMsg("New prototype : ready to use");
+                                setLoadingPage(false);
+                                setProtoUpdate(false);
+                            })
+                            .catch(error => {
+                                // setErrorMsg("Internal error");
+                                console.log("Error:", error);
+                            });
                     }
                     break;
                 case 'execute':
                     setLoadingPage(false);
                     const targetElem = document.querySelector('#placeholder');
                     if (!targetElem) return;
-                    targetElem.innerHTML = data["execute"]["success"] === 200 
+                    targetElem.innerHTML = data["execute"]["success"] === 200
                         ? data["execute"]["content"]
                         : `<p class='centered'>Http response error : ${data["execute"]["success"]}</p>`;
                     break;
@@ -138,7 +138,7 @@ export function Proto() {
     useEffect(() => {
         if (!(socket && socketUsable && pages.length >= 2)) return;
         setCurrentPage(pages[1].name);
-        socket.send(JSON.stringify({"action":"execute", "page": pages[1].link}))
+        socket.send(JSON.stringify({"action": "execute", "page": pages[1].link}))
     }, [socket, socketUsable, pages]);
 
     const [successMsg, setSuccessMsg] = useState<string>("");
@@ -154,7 +154,7 @@ export function Proto() {
         if (errorMsg) setTimeout(() => setErrorMsg(""), 4000);
     }, [errorMsg]);
 
-    function triggerLink({link, name}:{link:string, name:string}) {
+    function triggerLink({link, name}: { link: string, name: string }) {
         if (!socket || !socketUsable) return;
         if (protoUpdate) {
             setErrorMsg("Reloading prototype");
@@ -162,7 +162,7 @@ export function Proto() {
         }
         setLoadingPage(true);
         setCurrentPage(name);
-        socket.send(JSON.stringify({"action":"execute", "page": link}));
+        socket.send(JSON.stringify({"action": "execute", "page": link}));
     }
 
     function triggerResetSession() {
@@ -172,11 +172,11 @@ export function Proto() {
             return;
         }
         setInfoMsg("Send reset session request...");
-        socket.send(JSON.stringify({"action":"reset_session"}));
+        socket.send(JSON.stringify({"action": "reset_session"}));
     }
 
     useEffect(() => {
-        function handleLinks(event:Event) {
+        function handleLinks(event: Event) {
             let index = 0;
             while (index < event.composedPath().length && (event.composedPath()[index] as HTMLElement).tagName !== "A") ++index;
 
@@ -190,10 +190,12 @@ export function Proto() {
             if (!targetPage || !socket || !socketUsable) return;
 
             //TODO - update current page : if on pages, select it, else, select 404 (lignes 30 et 31)
+            setPages([{name: 'Ecran1', link: '/Ecran1'}]);
 
             setLoadingPage(true);
-            socket.send(JSON.stringify({"action":"execute", "page": targetPage}));
+            socket.send(JSON.stringify({"action": "execute", "page": targetPage}));
         }
+
         const targetElem = document.querySelector('#exec_window');
         targetElem?.addEventListener('click', handleLinks);
         return () => targetElem?.removeEventListener('click', handleLinks);
@@ -202,13 +204,19 @@ export function Proto() {
     return (
         <section id="proto">
             <div className='popup'>
-                { successMsg && <Fade><div className='success'>{successMsg}</div></Fade> }
-                { infoMsg && <Fade><div className='info'>{infoMsg}</div></Fade> }
-                { errorMsg && <Fade><div className='error'>{errorMsg}</div></Fade> }
+                {successMsg && <Fade>
+                    <div className='success'>{successMsg}</div>
+                </Fade>}
+                {infoMsg && <Fade>
+                    <div className='info'>{infoMsg}</div>
+                </Fade>}
+                {errorMsg && <Fade>
+                    <div className='error'>{errorMsg}</div>
+                </Fade>}
             </div>
             <div className='session'>
                 <div className="users">
-                    <Collabs usernames={[userContext.user.name+" (you)", ...loggedCollabs]} />
+                    <Collabs usernames={[userContext.user.name + " (you)", ...loggedCollabs]}/>
                 </div>
                 <h2>Server session</h2>
                 <div className='button' onClick={triggerResetSession}>Reset</div>
@@ -216,14 +224,15 @@ export function Proto() {
             </div>
             <div>
                 <div className='links'>
-                    { pages.map(item => <p 
-                        key={item.name} 
-                        className={item.name === currentPage ? "selected" : ""} 
+                    {pages.map(item => <p
+                        key={item.name}
+                        className={item.name === currentPage ? "selected" : ""}
                         onClick={() => triggerLink(item)}>{item.name}
-                    </p>) }
+                    </p>)}
                 </div>
                 <div id="exec_window">
-                    { socketUsable ? (loadingPage ? <p className='centered'>Loading...</p> : <div id="placeholder"/>) : <p className='centered'>Connection to server...</p> }
+                    {socketUsable ? (loadingPage ? <p className='centered'>Loading...</p> : <div id="placeholder"/>) :
+                        <p className='centered'>Connection to server...</p>}
                 </div>
             </div>
         </section>
