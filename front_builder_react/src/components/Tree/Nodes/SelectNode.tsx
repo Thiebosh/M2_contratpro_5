f6c7cd4106b90e4c3599ee1e97d1578a1ae3a831
Antoine <antoinea723@gmail.com>
2022-05-20 11:52:03 +0200

    Fix bug with missing props

--- front_builder_react/src/components/Tree/Nodes/SelectNode.tsx
+++ front_builder_react/src/components/Tree/Nodes/SelectNode.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import { updateValueOnNode } from "./../functions/node";
 import './styles.scss';
 
-export default function SelectNode({nodeDatum, updateSelect}: {nodeDatum: any, updateSelect: Function}) {
+export default function SelectNode({nodeDatum, updateSelect, tree, setTree, socket}: {nodeDatum: any, updateSelect: Function, tree:any, setTree:React.Dispatch<any>, socket:any}) {
   function handleSelect(e: any) {
+    updateValueOnNode(e.target.value, e.target.className, tree, setTree, socket);
     updateSelect(e.target.value)
   }
 
@@ -11,7 +12,7 @@ export default function SelectNode({nodeDatum, updateSelect}: {nodeDatum: any, u
     <g className={nodeDatum.path}>
       <foreignObject width={120} height={70} y={-35} x={-60}>
         <div className="node-div"><label>{nodeDatum.syntaxKey}</label>
-          <select onChange={handleSelect}>
+          <select onChange={handleSelect} className={nodeDatum.path+"_select"}>
             {nodeDatum.values.map((optionValue: any) => (
               <option key={optionValue} value={optionValue}>{optionValue}</option>
             ))}
